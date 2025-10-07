5f7659d0c1c5d093014fc3f3975d7b5dc1238f28
tatjanon <winfield@support.email.ne.jp>
2025-10-07 16:14:49 +0900

    Update cloud-init script for VM initialization
    Remove template maintenance configuration and set hostname preservation to false.

--- vSphere/cloudinit-vm-deploy/scripts/init-vm-cloudinit.sh
+++ vSphere/cloudinit-vm-deploy/scripts/init-vm-cloudinit.sh
@@ -4,3 +4,5 @@ subscription-manager remove --all
 cloud-init clean
 rm -f /etc/ssh/ssh_host_*
 truncate -s0 /etc/machine-id
+rm -f /etc/cloud/cloud.cfg.d/99-template-maint.conf
+echo "preserve_hostname: false" > /etc/cloud/cloud.cfg.d/99-override.conf
