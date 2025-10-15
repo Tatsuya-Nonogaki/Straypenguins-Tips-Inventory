8c75e442089903bf368b6152745bad10a04b3c5e
tatjanon <winfield@support.email.ne.jp>
2025-10-15 14:43:45 +0900

    Fix syntax for creating 99-override.cfg

--- vSphere/cloudinit-vm-deploy/scripts/init-vm-cloudinit.sh
+++ vSphere/cloudinit-vm-deploy/scripts/init-vm-cloudinit.sh
@@ -5,7 +5,7 @@ cloud-init clean
 truncate -s0 /etc/machine-id
 rm -f /etc/cloud/cloud.cfg.d/99-template-maint.cfg /etc/cloud/cloud-init.disabled
 # Create /etc/cloud/cloud.cfg.d/99-override.cfg for the clone
-cat > /etc/cloud/cloud.cfg.d/99-override.cfg <<EOM
+cat <<EOM >/etc/cloud/cloud.cfg.d/99-override.cfg
 preserve_hostname: false
 manage_etc_hosts: false
-EOM 
+EOM
