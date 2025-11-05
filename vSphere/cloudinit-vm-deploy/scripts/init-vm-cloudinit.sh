1a690bee2f8bf10c0b3896f1cf542d86fbf0a12d
tatjanon <winfield@support.email.ne.jp>
2025-11-05 15:55:04 +0900

    Add deletion of all NetworkManager Ethernet connections

--- vSphere/cloudinit-vm-deploy/scripts/init-vm-cloudinit.sh
+++ vSphere/cloudinit-vm-deploy/scripts/init-vm-cloudinit.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -x
 subscription-manager clean
 subscription-manager remove --all
+nmcli -t -f UUID,TYPE connection show 2>/dev/null | awk -F: '$2=="802-3-ethernet" { print $1 }' | while read UUID; do
+  [ -n "$UUID" ] && nmcli connection delete uuid "$UUID" >/dev/null 2>&1
+done
+nmcli connection reload >/dev/null 2>&1
 cloud-init clean
 truncate -s0 /etc/machine-id
 rm -f /etc/cloud/cloud.cfg.d/99-template-maint.cfg /etc/cloud/cloud-init.disabled
